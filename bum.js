aa167b10833d7842d0bf53bc4044746bcdcccb59
Lotosney <sebastiandwojcik@gmail.com>
2020-11-23 09:47:57 +0100

    Add some css and js

---
+++ bum.js
@@ -0,0 +1,21 @@
+var scoreNum = 0
+numRandom=()=>{
+    var num;
+    num = Math.floor(Math.random() * (100 - 3)) + 3
+    return num
+}
+
+var isThisBum = () => {
+
+    if (numRandom() % 3 === 0 || numRandom().toString().indexOf("3") != -1) {
+        return "bum"
+        
+
+    } else {
+        return "not bum"
+        // console.log("notbum")
+    }
+}
+
+
+
