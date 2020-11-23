921d11e2b300dbb3212a500b344a5e5d47d66ffe
Lotosney <sebastiandwojcik@gmail.com>
2020-11-23 15:55:01 +0100

    Add validation function to yes and no buttons

--- bum.js
+++ bum.js
@@ -1,25 +1,46 @@
 var scoreNum = 0
-numRandom=()=>{
-    var num;
-    num = Math.floor(Math.random() * (100 - 3)) + 3
-    return num
-}
 
+var num;
+num = Math.floor(Math.random() * (100 - 3)) + 3
+
+console.log(num)
 var isThisBum = () => {
 
-    if (numRandom() % 3 === 0 || numRandom().toString().indexOf("3") != -1) {
+    if (num % 3 === 0 || num.toString().includes(3)) {
         return "bum"
-        
+
 
     } else {
         return "not bum"
 
     }
 }
+console.log(isThisBum())
+validateYes = () => {
+    if (isThisBum() === "bum") {
+        alert("Bum")
 
+
+
+    } else {
+        alert("You are wrong")
+
+    }
+
+}
+
+validateNo = () => {
+    if (isThisBum() === "not bum") {
+        alert("Not bum")
+
+
+    } else {
+        alert("You are wrong")
+    }
+
+}
 var number = document.getElementById('numberContainer')
-number.innerText = `${numRandom()}`
+number.innerText = `${num}`
 
 var scoreNumber = document.getElementById('scoreNumberContainer')
 scoreNumber.innerText = `${scoreNum}`
-
