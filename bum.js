a11c3da6b7b99878fd4f77adeac726ecedecc193
Lotosney <sebastiandwojcik@gmail.com>
2020-11-26 12:53:17 +0100

    Add validation, score update, random nr refresh

--- bum.js
+++ bum.js
@@ -2,15 +2,23 @@ var scoreNum = 0
 
 var num;
 num = Math.floor(Math.random() * (100 - 3)) + 3
+var number = document.getElementById('numberContainer')
+number.innerText = `${num}`
 
+var scoreNumber = document.getElementById('scoreNumberContainer')
+scoreNumber.innerText = `${scoreNum}`
 console.log(num)
 var isThisBum = () => {
 
     if (num % 3 === 0 || num.toString().includes(3)) {
+        num = Math.floor(Math.random() * (100 - 3)) + 3;
+        number.innerText = `${num}`;
         return "bum"
 
 
     } else {
+        num = Math.floor(Math.random() * (100 - 3)) + 3;
+        number.innerText = `${num}`;
         return "not bum"
 
     }
@@ -19,7 +27,8 @@ console.log(isThisBum())
 validateYes = () => {
     if (isThisBum() === "bum") {
         alert("Bum")
-
+        scoreNum=scoreNum+1
+        scoreNumber.innerText = `${scoreNum}`
 
 
     } else {
@@ -32,6 +41,8 @@ validateYes = () => {
 validateNo = () => {
     if (isThisBum() === "not bum") {
         alert("Not bum")
+        scoreNum=scoreNum+1
+        scoreNumber.innerText = `${scoreNum }`
 
 
     } else {
@@ -39,8 +50,3 @@ validateNo = () => {
     }
 
 }
-var number = document.getElementById('numberContainer')
-number.innerText = `${num}`
-
-var scoreNumber = document.getElementById('scoreNumberContainer')
-scoreNumber.innerText = `${scoreNum}`
